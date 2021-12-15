ee27afe99f6a5d774d10f98970ba1fd7d36b7ddd
swapnil810 <sharmaswapnil810@gmail.com>
2021-12-15 08:46:15 +0530

    performed 16,9,25 operations

--- app/src/main/java/com/example/mcc/Task2.kt
+++ app/src/main/java/com/example/mcc/Task2.kt
@@ -14,6 +14,7 @@ class Task2 : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task2)
 
+        //this method is used to pass current context of the click operation
         et_input.setOnClickListener(this)
         btn_i.setOnClickListener(this)
         lin_25_operation.setOnClickListener(this)
@@ -23,37 +24,29 @@ class Task2 : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            R.id.et_input -> {
-                myToast("Button One Clicked")
-            }
             R.id.btn_i -> {
                 val inputText = et_input.text.toString().trim();
 
                 if (inputText.equals("9")) {
                     lin_1st_operation.isVisible = true
                     lin_16st_operation.isVisible = false
-                    lin_25_operation.isVisible=false
-
+                    lin_25_operation.isVisible = false
 
                 } else if (inputText.equals("16")) {
-
                     lin_16st_operation.isVisible = true
                     lin_1st_operation.isVisible = false
-                    lin_25_operation.isVisible=false
-
-
+                    lin_25_operation.isVisible = false
 
                 } else if (inputText.equals("25")) {
                     lin_16st_operation.isVisible = false
                     lin_1st_operation.isVisible = false
-                    lin_25_operation.isVisible=true
-
+                    lin_25_operation.isVisible = true
 
                 } else {
                     myToast("please enter 16 , 9, 25")
                     lin_1st_operation.isVisible = false
                     lin_16st_operation.isVisible = false
-                    lin_25_operation.isVisible=false
+                    lin_25_operation.isVisible = false
 
 
                 }

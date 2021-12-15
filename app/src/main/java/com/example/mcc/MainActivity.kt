c9cc116d73150338e8e1667e28bed4b2418fdbc4
swapnil810 <sharmaswapnil810@gmail.com>
2021-12-15 08:44:47 +0530

    performed 16,9,25 operations

--- app/src/main/java/com/example/mcc/MainActivity.kt
+++ app/src/main/java/com/example/mcc/MainActivity.kt
@@ -1,26 +1,33 @@
 package com.example.mcc
 
 import Data
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //gh
         setContentView(R.layout.activity_main)
+        imageView.setOnClickListener(this)
 
 
         //calling web services
         val apiInterface = ApiInterface.create().getResultData()
-        apiInterface.enqueue( object : Callback<List<Result<Data>>> {
-            override fun onResponse(call: Call<List<Result<Data>>>?, response: Response<List<Result<Data>>>?) {
+        apiInterface.enqueue(object : Callback<List<Result<Data>>> {
+            override fun onResponse(
+                call: Call<List<Result<Data>>>?,
+                response: Response<List<Result<Data>>>?
+            ) {
 
-                Log.e("TAG", "onResponse: "+response, )
+                Log.e("TAG", "onResponse: " + response)
 
             }
 
@@ -30,5 +37,16 @@ class MainActivity : AppCompatActivity() {
         })
 
 
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.imageView -> {
+                //go to 1st task screen
+                val intent = Intent(this@MainActivity,Task2::class.java);
+                startActivity(intent);
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/mcc/Task2.kt
+++ app/src/main/java/com/example/mcc/Task2.kt
@@ -4,9 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_task2.*
 
-class Task2 : AppCompatActivity(),View.OnClickListener {
+class Task2 : AppCompatActivity(), View.OnClickListener {
     private var toast: Toast? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,28 +16,46 @@ class Task2 : AppCompatActivity(),View.OnClickListener {
 
         et_input.setOnClickListener(this)
         btn_i.setOnClickListener(this)
-
+        lin_25_operation.setOnClickListener(this)
+        lin_1st_operation.setOnClickListener(this)
+        lin_16st_operation.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
-        when ( v!!.id)
-        {
-            R.id.et_input->
-            {
+        when (v!!.id) {
+            R.id.et_input -> {
                 myToast("Button One Clicked")
             }
-            R.id.btn_i->
-            {
-                val inputText =et_input.text.toString().trim();
+            R.id.btn_i -> {
+                val inputText = et_input.text.toString().trim();
 
-                if(inputText.equals("9")){
+                if (inputText.equals("9")) {
+                    lin_1st_operation.isVisible = true
+                    lin_16st_operation.isVisible = false
+                    lin_25_operation.isVisible=false
 
-                }else if(inputText.equals("16")){
 
-                }else if (inputText.equals("25")){
+                } else if (inputText.equals("16")) {
 
-                }else{
+                    lin_16st_operation.isVisible = true
+                    lin_1st_operation.isVisible = false
+                    lin_25_operation.isVisible=false
+
+
+
+                } else if (inputText.equals("25")) {
+                    lin_16st_operation.isVisible = false
+                    lin_1st_operation.isVisible = false
+                    lin_25_operation.isVisible=true
+
+
+                } else {
                     myToast("please enter 16 , 9, 25")
+                    lin_1st_operation.isVisible = false
+                    lin_16st_operation.isVisible = false
+                    lin_25_operation.isVisible=false
+
+
                 }
             }
 
@@ -44,9 +63,8 @@ class Task2 : AppCompatActivity(),View.OnClickListener {
 
     }
 
-    private fun myToast(string: String)
-    {
-        toast = Toast.makeText(this,string,Toast.LENGTH_SHORT)
+    private fun myToast(string: String) {
+        toast = Toast.makeText(this, string, Toast.LENGTH_SHORT)
         toast?.show()
     }
 
